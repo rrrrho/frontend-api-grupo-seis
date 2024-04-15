63f3ec6f1113fbb61cc6f46cb6f04fa8c0cd2974
jgervasio1 <juliangervasio1@gmail.com>
2024-04-15 20:21:19 -0300

    subo lo hecho en local

--- api-grupo-seis/src/App.jsx
+++ api-grupo-seis/src/App.jsx
@@ -2,13 +2,24 @@ import './App.css';
 import Home from "./components/Home";
 import { Routes, Route } from "react-router-dom";
 import Layout from './components/Layout';
+import ProductDetail from './components/ProductDetail';
 
 function App() {
+  const product = {
+    image: 'https://arcordiezb2c.vteximg.com.br/arquivos/ids/160297/Raza-Carne-Para-Perro-x-3-Kg-1-2427.jpg?v=637450400824130000',
+    nombre : 'Comida Para Perros',
+    description: 'Descripción del producto',
+    price: 100,
+    category: 'Categoría del producto',
+    seller: 'Nombre del vendedor',
+    stock: 1,
+  };
 
   return (
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/product-detail" element={<ProductDetail product={product} />} />
         </Routes>
       </Layout>
   )
---
+++ api-grupo-seis/src/components/ProductDetail/index.jsx
@@ -0,0 +1,31 @@
+import { Box, Flex, Image, Text } from '@chakra-ui/react';
+
+const ProductDetail = ({ product }) => {
+  const { image, nombre , description, price, category, seller, stock } = product;
+
+  return (
+    <Flex justify="center" align="center" mt="2rem">
+      <Box maxW="80%" p="2rem" borderWidth="1px" borderRadius="xl" boxShadow="md" backgroundColor="#FFF6EA">
+        <Flex justify="center" align="flex-start">
+          <Box mr="4rem">
+            <Image src={image} alt={description} w="20rem" h="auto" />
+          </Box>
+          <Box>
+            <Text fontSize="2.5rem" fontWeight="bold" mb="2rem">{nombre}</Text>
+            <Text fontSize="1.5rem" fontWeight="bold" mb="2rem">{description}</Text>
+            <Text fontSize="1.5rem" fontWeight="bold" mb="3rem"><strong>Precio:</strong> ${price}</Text>
+            <Text fontSize="1.2rem"><strong>Vendedor:</strong> {seller}</Text>
+            <Flex align="center" mt="2rem">
+              <Text fontSize="1.5rem" mr="2rem">Cantidad:</Text>
+              <input type="number" min="1" max={stock} defaultValue="1" style={{ width: '6rem', padding: '0.5rem', borderRadius: '0.5rem', border: '1px solid #ddd', marginRight: '1rem' }} />
+              <Text fontSize="1.5rem" color={stock > 0 ? "green.500" : "red.500"}>{stock > 0 ? `Disponible: ${stock} unidades` : "Agotado"}</Text>
+            </Flex>
+            <button style={{ backgroundColor: '#7D5A50', color: 'white', border: 'none', borderRadius: '0.5rem', cursor: 'pointer', padding: '1rem 2rem', marginTop: '1rem' }} disabled={stock === 0}>Agregar al carrito</button>
+          </Box>
+        </Flex>
+      </Box>
+    </Flex>
+  );
+};
+
+export default ProductDetail;
