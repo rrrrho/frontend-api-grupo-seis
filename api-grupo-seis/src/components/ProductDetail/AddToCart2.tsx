95fbd3c0dd3a18dd042334b487462a566f5154f3
jgervasio1 <juliangervasio1@gmail.com>
2024-05-06 20:19:14 -0300

    linkeo las cards con el product detail

---
+++ api-grupo-seis/src/components/ProductDetail/AddToCart2.tsx
@@ -0,0 +1,53 @@
+import { Button, useDisclosure } from "@chakra-ui/react";
+import { useAppDispatch, useAppSelector } from "../../context/hooks";
+import { addItem } from "../../context/slices/cartSlice";
+import { Product } from "../../types/product";
+import React from "react";
+import ModalSuccess from "../Modal/ModalSuccess";
+import ModalError from "../Modal/ModalError";
+
+const AddToCart = ({id, name, price, image, rating, voters, discount, quota, stock, bestseller}: Product) => {
+    const items = useAppSelector((state) => state.cart.items);
+    const dispatch = useAppDispatch();
+    const { isOpen: isOpenSuccess, onOpen: onOpenSuccess, onClose: onCloseSuccess } = useDisclosure();
+    const { isOpen: isOpenError, onOpen: onOpenError, onClose: onCloseError } = useDisclosure();
+
+    const addItemToCart = () => {
+        const isItemInCart = items.some((item) => item.product.id === id);
+        const product = {id, name, price, image, rating, voters, discount, quota, stock, bestseller};
+    
+        if (!isItemInCart) {
+            dispatch(addItem({ product, quantity: 1 }));
+            onOpenSuccess();
+        } else {
+            onOpenError();
+        };
+        
+    };
+
+    return (
+        <>
+            <Button 
+                position="relative" 
+                bottom={{base: '46%', xl: "45%"}} 
+                top="8rem"
+                left="55%" 
+                bg="#E8C07D" 
+                color="#4E6E52"
+                w={{base: '8rem', xl: "10rem"}} 
+                h={{base: '3rem', xl: "3.5rem"}} 
+                cursor="pointer" 
+                onClick={()=> addItemToCart()} 
+                _hover="none"
+                borderRadius="10"
+            >
+                Agregar al carrito
+            </Button>
+
+            <ModalSuccess isOpen={isOpenSuccess} onClose={onCloseSuccess} title="¡Se agregó el producto al carrito!" />
+            <ModalError isOpen={isOpenError} onClose={onCloseError} title="Ya existe este producto en el carrito." />
+        </>
+    );
+};
+
+export default AddToCart;
