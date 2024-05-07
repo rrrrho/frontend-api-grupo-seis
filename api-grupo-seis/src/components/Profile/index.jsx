3c0a50232ef705118a7f49178c1ea0ca1090b77e
pepi <fedeabeledo01@gmail.com>
2024-05-07 20:26:52 -0300

    add loading in profile + add radio buttons in register

--- api-grupo-seis/src/components/Profile/index.jsx
+++ api-grupo-seis/src/components/Profile/index.jsx
@@ -1,44 +1,55 @@
 import { Box } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 import SeccionPerfil from "./Perfil";
 import PedidosRealizados from "./Pedidos";
 import ProductForm from "../ProductForm/ProductForm";
 import UserAdmin from "../UserAdmin/UserAdmin";
 import ManageProductTable from "../ManageProducts/ManageProduct";
+import Loading from "../Loading/Loading";
 
 const Profile = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
   return (
-    <div>
-      <Box w={"100%"} background={"brand.darkMustard"} h={"10vh"} />
+    <>
+      {isLoading && <Loading />}
+      <div>
+        <Box w={"100%"} background={"brand.darkMustard"} h={"10vh"} />
 
-      <Tabs isFitted variant="enclosed" colorScheme="green">
-        <TabList mb="1em">
-          <Tab>Perfil</Tab>
-          <Tab>Pedidos</Tab>
-          <Tab>Agregar producto</Tab>
-          <Tab>Gestionar productos</Tab>
-          <Tab>Administración de usuarios</Tab>
-        </TabList>
+        <Tabs isFitted variant="enclosed" colorScheme="green">
+          <TabList mb="1em">
+            <Tab>Perfil</Tab>
+            <Tab>Pedidos</Tab>
+            <Tab>Agregar producto</Tab>
+            <Tab>Gestionar productos</Tab>
+            <Tab>Administración de usuarios</Tab>
+          </TabList>
 
-        <TabPanels>
-          <TabPanel>
-            <SeccionPerfil />
-          </TabPanel>
-          <TabPanel>
-            <PedidosRealizados />
-          </TabPanel>
-          <TabPanel>
-            <ProductForm />
-          </TabPanel>
-          <TabPanel>
-            <ManageProductTable />
-          </TabPanel>
-          <TabPanel>
-            <UserAdmin />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </div>
+          <TabPanels>
+            <TabPanel>
+              <SeccionPerfil />
+            </TabPanel>
+            <TabPanel>
+              <PedidosRealizados />
+            </TabPanel>
+            <TabPanel>
+              <ProductForm />
+            </TabPanel>
+            <TabPanel>
+              <ManageProductTable />
+            </TabPanel>
+            <TabPanel>
+              <UserAdmin />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </div>
+    </>
   );
 };
 
