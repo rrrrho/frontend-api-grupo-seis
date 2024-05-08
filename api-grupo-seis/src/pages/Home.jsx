eacf6559ea65bcfca3a29f0808b4fb5ea0d5846a
rrrrho <rocioghillino@gmail.com>
2024-05-07 21:16:18 -0300

    added skeleton to home page

--- api-grupo-seis/src/pages/Home.jsx
+++ api-grupo-seis/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Image, Grid, GridItem, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image, Grid, GridItem, Text, Skeleton } from "@chakra-ui/react";
 import Banner from "/src/components/Home/Banner";
 import dogImage from "/src/assets/img/home/dog.png"; 
 import catImage from "/src/assets/img/home/cat.png"; 
@@ -73,36 +73,52 @@ const Home = () => {
         <>
             {isLoading && <Loading />}
             <Flex w={'100vw'} mb="4rem" flexDir={'column'} gap={20}>
-                <Banner/>
+                <Skeleton isLoaded={!isLoading} startColor='brand.darkGreen' endColor='brand.lightGreen'>
+                    <Banner/>
+                </Skeleton>
+                
                 <Grid 
                 w={'92vw'} 
                 templateRows='repeat(2, 1fr)' 
                 templateColumns='repeat(14, 1fr)' 
                 alignSelf={'center'}
                 gap={5}>
-
-                    <GridItem bg={'brand.darkGreen'} rowSpan={2} py={10} colSpan={8} borderRadius={10} display={'flex'} flexDir={'column'} justifyContent={'center'} alignItems={'center'}>
-                        <Box>
-                            <Box w={'28vw'}>
-                                <Heading fontSize={'3.5rem'} color={'brand.lightBeige'}>Nuestras categorias</Heading>
-                                <Text color={'brand.lightBeige'}>Descubre nuestra variedad de categorías para mascotas y encuentra todo lo que necesitas para consentir a tu compañero peludo en un solo lugar.</Text>
-                            </Box>
-                            <Categories items={categories}/>
-                        </Box>
-                    </GridItem>
-
-                    <GridItem rowSpan={2} colSpan={6} >
-                        <InfoCards cards={cards}/>
-                    </GridItem>
                     
+                    <Skeleton 
+                    as={GridItem} 
+                    isLoaded={!isLoading} 
+                    startColor='brand.darkGreen' 
+                    endColor='brand.lightGreen' 
+                    bg={'brand.darkGreen'} 
+                    rowSpan={2} 
+                    py={10} 
+                    colSpan={8} 
+                    borderRadius={10} 
+                    display={'flex'} 
+                    flexDir={'column'} 
+                    justifyContent={'center'} 
+                    alignItems={'center'}>
+                    <Box>
+                        <Box w={'28vw'}>
+                            <Heading fontSize={'3.5rem'} color={'brand.lightBeige'}>Nuestras categorias</Heading>
+                            <Text color={'brand.lightBeige'}>Descubre nuestra variedad de categorías para mascotas y encuentra todo lo que necesitas para consentir a tu compañero peludo en un solo lugar.</Text>
+                        </Box>
+                        <Categories items={categories}/>
+                    </Box>
+                    </Skeleton>
+
+                    <Skeleton as={GridItem} rowSpan={2} colSpan={6} isLoaded={!isLoading} startColor='brand.darkGreen' endColor='brand.lightGreen'>
+                        <InfoCards cards={cards}/>
+                     </Skeleton>
                 </Grid>
-                    <Flex overflow={'hidden'} py={10} display={'flex'} justifyContent={'center'} pos={'relative'}>
-                        <Image src={blobOne} pos={'absolute'} right={'90%'} boxSize={'30rem'} top={5}/>
-                        <Image src={blobTwo} pos={'absolute'} left={'87%'} boxSize={'25rem'} bottom={0}/>
-                        <Image src={blobThree} pos={'absolute'} left={'75%'} boxSize={'15rem'} top={'5%'}/>
-                        <Image src={blobFour} pos={'absolute'} right={'75%'} boxSize={'15rem'} bottom={0}/>
-                        <BestSellers products={bestsellers} title={'¡Los mas vendidos!'} description={'Descubre los artículos imprescindibles para tu mascota. Desde juguetes interactivos hasta cómodas camas, encuentra todo lo que necesitas para cuidar y mimar a tu compañero peludo. ¡Calidad garantizada y precios irresistibles!'}/>
-                    </Flex>
+                
+                <Skeleton as={Flex} isLoaded={!isLoading} startColor='brand.darkGreen' endColor='brand.lightGreen' overflow={'hidden'} py={10} display={'flex'} justifyContent={'center'} pos={'relative'}>
+                    <Image src={blobOne} pos={'absolute'} right={'90%'} boxSize={'30rem'} top={5}/>
+                    <Image src={blobTwo} pos={'absolute'} left={'87%'} boxSize={'25rem'} bottom={0}/>
+                    <Image src={blobThree} pos={'absolute'} left={'75%'} boxSize={'15rem'} top={'5%'}/>
+                    <Image src={blobFour} pos={'absolute'} right={'75%'} boxSize={'15rem'} bottom={0}/>
+                    <BestSellers products={bestsellers} title={'¡Los mas vendidos!'} description={'Descubre los artículos imprescindibles para tu mascota. Desde juguetes interactivos hasta cómodas camas, encuentra todo lo que necesitas para cuidar y mimar a tu compañero peludo. ¡Calidad garantizada y precios irresistibles!'}/>
+                </Skeleton>
             </Flex>  
         </>
     );
